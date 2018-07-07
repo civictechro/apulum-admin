5c3ea73e9f70d28d896d46c5579d19bb092616bf
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-07-08 02:56:45 +0300

    fix(precommit) add jest config

--- config-overrides.js
+++ config-overrides.js
@@ -3,40 +3,42 @@ const tsImportPluginFactory = require('ts-import-plugin')
 const { getLoader, injectBabelPlugin } = require("react-app-rewired");
 const rewireLess = require('react-app-rewire-less');
 
-module.exports = function override(config, env) {
-  const tsLoader = getLoader(
-    config.module.rules,
-    rule =>
-      rule.loader &&
-      typeof rule.loader === 'string' &&
-      rule.loader.includes('ts-loader')
-  );
+module.exports = {
+  webpack: function override(config, env) {
+    const tsLoader = getLoader(
+      config.module.rules,
+      rule =>
+        rule.loader &&
+        typeof rule.loader === 'string' &&
+        rule.loader.includes('ts-loader')
+    );
 
-  tsLoader.options = {
-    getCustomTransformers: () => ({
-      before: [ tsImportPluginFactory({
-        libraryDirectory: 'es',
-        libraryName: 'antd',
-        style: true,
-      }) ]
-    })
-	};
+    tsLoader.options = {
+      getCustomTransformers: () => ({
+        before: [ tsImportPluginFactory({
+          libraryDirectory: 'es',
+          libraryName: 'antd',
+          style: true,
+        }) ]
+      })
+    };
 
-	/*
-  tsLoader.options = {
-    getCustomTransformers: () => ({
-      before: [ tsImportPluginFactory({
-				camel2DashComponentName: false,
-        libraryDirectory: 'lib',
-        libraryName: 'ant-design-pro',
-				style: true,
-      }) ]
-    })
-	};*/
 
-	config = rewireLess.withLoaderOptions({
-		javascriptEnabled: true,
-	})(config, env);
+  /*   tsLoader.options = {
+      getCustomTransformers: () => ({
+        before: [ tsImportPluginFactory({
+          camel2DashComponentName: false,
+          libraryDirectory: 'lib',
+          libraryName: 'ant-design-pro',
+          style: true,
+        }) ]
+      })
+    }; */
 
-  return config;
+    config = rewireLess.withLoaderOptions({
+      javascriptEnabled: true,
+    })(config, env);
+
+    return config;
+  }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,38 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
 
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from "./registerServiceWorker";
 
-import { fragmentMatcher } from './utils/fragmentMatcher';
-import { Routes } from './routes';
+import { fragmentMatcher } from "./utils/fragmentMatcher";
+import { Routes } from "./routes";
+
+import "./index.css";
  
-import './index.css';
-
 const client = new ApolloClient({
   link: ApolloLink.from([
     onError(({ graphQLErrors, networkError }) => {
       if (graphQLErrors) {
         graphQLErrors.map(({ message, locations, path }) =>
           console.log(
-            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
-          ),
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
         );
       }
 
-      if (networkError: number) {
+      if (networkError) {
         console.log(`[Network error]: ${networkError}`);
       }
     }),
     new HttpLink({
       uri: process.env.REACT_APP_GRAPHQL_HOST,
-      credentials: 'include'
+      credentials: "include"
     })
   ]),
   cache: new InMemoryCache({ fragmentMatcher })
@@ -44,8 +44,5 @@ const WrappedApp = (
   </ApolloProvider>
 );
 
-ReactDOM.render(
-  WrappedApp,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(WrappedApp, document.getElementById("root") as HTMLElement);
 registerServiceWorker();
