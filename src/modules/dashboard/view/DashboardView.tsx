cbdce0ef250a8b4263bb1cb262e7f7c1a0934135
ClaudiuCeia <claudiu.ceia+1@gmail.com>
2018-11-01 02:34:04 +0200

    fix(incidents) add filter menu by incident type

--- src/modules/dashboard/view/DashboardView.tsx
+++ src/modules/dashboard/view/DashboardView.tsx
@@ -44,6 +44,8 @@ export default class DashboardView extends React.PureComponent<Props, {}> {
       me
     } = this.props.data.dashboardQuery;
 
+    console.log(me);
+
     if (loading) {
       return <Card loading={ true } />;
     }
